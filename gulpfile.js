b2777bf37f4535159afa3f3358aabd2fd7079fe4
Marcus Russi <marcusrussi@gmail.com>
2016-07-29 17:03:08 -0700

    Beginning to write .jade templates

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var reactify = require('reactify');
+var jade = require('gulp-jade');
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './main.js',
+    debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [babelify]
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build/js/'));
+});
+
+
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+
+  gulp.src('./**/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./build/'));
+});
